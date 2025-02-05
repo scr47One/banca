fd84bfdc9629c52d6a7691c1332cccc05abc7841
jakoujan <jakoujan@gmail.com>
2025-02-05 10:56:59 -0600

    se agrega servicio para manipulación de transacción

--- src/app/services/account-manager.service.ts
+++ src/app/services/account-manager.service.ts
@@ -1,14 +1,43 @@
 import { Injectable } from '@angular/core';
-import { LocalStorageService } from 'ngx-webstorage';
-import { IAccount } from '../entities/interfaces';
+import { Observable } from 'rxjs';
 import { TranscationType } from '../entities/enums';
+import { IAccount } from '../entities/interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountManagerService {
 
-  constructor(private storage: LocalStorageService) { }
+  constructor() { }
 
-
+  public addTrasaction(account: IAccount, amount: number, transactionType: TranscationType): Observable<IAccount> {
+    return new Observable(observer => {
+      if (transactionType === TranscationType.DEPOSIT) {
+        account.balance += amount;
+      } else if (transactionType === TranscationType.WITHDRAW) {
+        if (account.balance >= amount) {
+          account.balance -= amount;
+        } else {
+          observer.error('Insufficient funds');
+          return;
+        }
+      } else if (transactionType === TranscationType.TRANSFER) {
+        if (account.balance >= amount) {
+          account.balance -= amount;
+        } else {
+          observer.error('Insufficient funds');
+          return;
+        }
+      }
+      account.transactions.push({
+        transactionId: Math.random().toString(36).substring(2, 9),
+        accountId: account.accountId,
+        amount: amount,
+        transactionDate: new Date(),
+        transactionType: transactionType
+      });
+      observer.next(account);
+      observer.complete();
+    });
+  }
 }
