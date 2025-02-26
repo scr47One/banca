25adae57fd34f06b5ed4ada93a879e33b88b0643
scr47One <hgonzalezg@toluca.tecnm.mx>
2025-02-26 17:38:52 -0600

    feat: update account balance feature with improved scenario descriptions and integrate Selenium for testing

--- features/accounts_balance.feature
+++ features/accounts_balance.feature
@@ -1,5 +1,4 @@
 Feature: Balance de cuentas
-  Scenario: Suma del balance de dos cuentas corrientes en el mismo tipo de cambio
-    Given dos cuentas con balance de 50000 y 25000
-    When sumamos el balance de ambas
-    Then el resultado debe ser de 75000
+  Scenario: Verificar la suma de balance de cuentas
+    Given dos cuentas con balance de MX$50,000.00 y MX$25,000.00
+    Then el resultado debe ser de 'MX$75,000.00'
--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,35 +1,23 @@
 const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
+const { Builder, By, Browser, WebDriver, until } = require('selenium-webdriver');
 
-function pipe (accounts) {
-  return accounts.reduce((acc, account) => acc + account.balance, 0);
+/**
+ * @type WebDriver
+ */
+let driver;
+
+async function init() {
+    driver = new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get('http://127.0.0.1:4200');
 }
 
-Given('dos cuentas con balance de 50000 y 25000', function () {
-  this.accounts = [
-    {
-      accountId: '',
-      balance: 50000,
-      accountName: 'Cuenta 1',
-      currency: 'MXN',
-      customerId: '1',
-      transactions: []
-    },
-    {
-      accountId: '',
-      balance: 25000,
-      accountName: 'Cuenta 2',
-      currency: 'MXN',
-      customerId: '1',
-      transactions: []
-    }
-  ]
-});
-  
-When('sumamos el balance de ambas', async function () {
-  this.result = parseInt(pipe(this.accounts));
+Given('dos cuentas con balance de MX$50,000.00 y MX$25,000.00', async function () {
+    await init();
+    this.result = await (await driver.wait(until.elementIsVisible(driver.findElement(By.id('accountBalance'))))).getText()
+    await driver.quit();
 });
 
-Then('el resultado debe ser de {int}', function (expectedAnswer) {
+Then('el resultado debe ser de {string}', async function (expectedAnswer) {
     assert.strictEqual( this.result, expectedAnswer);
 });
